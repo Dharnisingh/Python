f56bd9733f597d23e339ac3e493fa8cc1f994f54
Dharni <dharnisingh@gmail.com>
2020-07-19 12:56:21 +0530

    First Commit

---
+++ test.py
@@ -0,0 +1,8 @@
+print("This program will create a new file and write some content to tat\n")
+
+with open("new_fil",'w+') as f:
+    f.writelines("Hello this is new file\n")
+    f.writelines("How are you doing\n")
+    f.writelines("Lets see how it goes")
+
+print("Writing is done")
