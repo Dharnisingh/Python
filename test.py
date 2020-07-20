e5f1b12b38987331f86b075ea133a0c4f5824cae
Dharni <dharnisingh@gmail.com>
2020-07-20 15:43:32 +0530

    I am done

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 print("This program will create a new file and write some content to tat\n")
 
-with open("new_fil",'w+') as f:
+with open("/home/new_fil",'w+') as f:
     f.writelines("Hello this is new file\n")
     f.writelines("How are you doing\n")
     f.writelines("Lets see how it goes")
